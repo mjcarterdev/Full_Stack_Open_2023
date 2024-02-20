7d59bcdd63635b7150dc2c3ac9a3a33fbdc4e978
Michael Carter <michael_carter_21@hotmail.com>
2024-02-20 18:21:14 +0200

    exercise 1.11 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,49 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, bad, neutral }) => {
+  const average = (good - bad) / (good + neutral + bad);
+  const positive = (good / (good + neutral + bad)) * 100;
+  return (
+    <>
+      <h1>Statistics</h1>
+      {good + neutral + bad > 0 ? (
+        <table>
+          <tbody>
+            <StatisticLine text='good' value={good} />
+            <StatisticLine text='neutral' value={neutral} />
+            <StatisticLine text='bad' value={bad} />
+            <StatisticLine text='all' value={good + neutral + bad} />
+            <StatisticLine
+              text='average'
+              value={isNaN(average) ? 0 : average}
+            />
+            <StatisticLine
+              text='positive'
+              value={isNaN(positive) ? 0 : `${positive}%`}
+            />
+          </tbody>
+        </table>
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
@@ -13,12 +52,9 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <br />
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
